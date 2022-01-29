63e11e0ff2099fbb2fc23adfed01d60be2081f67
YeshayahuBennun <ybennun@yahoo.com>
2022-01-29 20:57:45 +0200

    commit#2 - Shared Preferences - How to Use Shared Preferences

--- app/src/main/java/com/ybennun/sprefs/MainActivity.java
+++ app/src/main/java/com/ybennun/sprefs/MainActivity.java
@@ -2,13 +2,50 @@ package com.ybennun.sprefs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String MESSAGE_ID = "messages_prefs";
+    private Button saveButton;
+    private EditText enterMessage;
+    private TextView showMessageTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        saveButton = findViewById(R.id.button);
+        enterMessage = findViewById(R.id.message_text_editText);
+        showMessageTextView = findViewById(R.id.show_message_textView);
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+                String message = enterMessage.getText().toString().trim();
+
+                SharedPreferences sharedPreferences = getSharedPreferences(MESSAGE_ID, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("message",message);
+
+                editor.apply();//saving to disk
+            }
+        });
+
+        //Get data back from SP
+        SharedPreferences getSharedData = getSharedPreferences(MESSAGE_ID,MODE_PRIVATE);
+        String value = getSharedData.getString("message","Nothing yet");
+
+        showMessageTextView.setText(value);
+
     }
 }
